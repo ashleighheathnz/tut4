baaa276f156707bae11cb1d965c5c17827977ae8
Ashleigh H <ashleighheathnz@gmail.com>
2022-08-18 10:00:18 +1200

    Subtract test failed

---
+++ src/test/java/org/example/SubtractTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SubtractTest {
+    @Test
+    void testAddition() {
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4,2));
+    }
+}
